1d3a4a601b5bb2f56b6782fd9cde21d177cf22e7
10234567Z <93607971+10234567Z@users.noreply.github.com>
2022-12-10 21:53:53 +0530

    Added game to UI/DOM

--- javascript.js
+++ javascript.js
@@ -1,7 +1,12 @@
 let choices = ['rock', 'paper', 'scissor'];
-let roundWinner;
+let roundWinner
+, computerPoints = 0
+, playerPoints = 0;
 
-const pageBackground = document.querySelector('.container');
+let playerScore = document.querySelector('.PlayerScore');
+let computerScore = document.querySelector('.ComputerScore');
+playerScore.textContent = playerPoints;
+computerScore.textContent = computerPoints;
 
 function getComputerChoice() {
     /**
@@ -13,101 +18,111 @@ function getComputerChoice() {
      * by this the number cant exceed past the maximum index number of array
      */
     let computerRandomChoice = Math.floor(Math.random() * choices.length);
-    // console.log(choices[computerRandomChoice]); // Debugging purposes
+    console.log(choices[computerRandomChoice]); // Debugging purposes
     return choices[computerRandomChoice].toString();
 }
 function getPlayerChoice(value) {
     let playerChoice = value;
-    console.log(playerChoice);
+    playRound(playerChoice,getComputerChoice());
 }
 
 function playRound(playerSelection, computerSelection) {
 
-    // What if the playerInput is not in the choices array 
-    if (!choices.includes(playerSelection)) {
-        roundWinner = "Please enter something only among rock , paper or scissor.";
-    }
+    // // What if the playerInput is not in the choices array 
+    // if (!choices.includes(playerSelection)) {
+    //     roundWinner = "Please enter something only among rock , paper or scissor.";
+    // }
     // What if the computer and player choices are same
-    else if (playerSelection == computerSelection) {
-        roundWinner = "Its a draw!";
+    if (playerSelection == computerSelection) {
+        roundWinner = "Draw";
     }
-    // What if player choose rock and computer choose paper
-    else if (playerSelection == choices[0] && computerSelection == choices[1]) {
-        roundWinner = "You lost against the paper!";
+    // What if player choose rock and computer choeos paper
+    else if (playerSelection == 'rock' && computerSelection == choices[1]) {
+        roundWinner = "Computer";
+        console.log(roundWinner);
     }
     // What if player choose rock and computer choose scissor
-    else if (playerSelection == choices[0] && computerSelection == choices[2]) {
-        roundWinner = "WHOA rock on scissor! gratz on win dud";
+    else if (playerSelection == 'rock' && computerSelection == choices[2]) {
+        roundWinner = "Player";
+        console.log(roundWinner);
     }
     // What if player choose paper and computer choose rock
-    else if (playerSelection == choices[1] && computerSelection == choices[0]) {
-        roundWinner = "WHOA! you win by catching the rock";
+    else if (playerSelection == 'paper' && computerSelection == choices[0]) {
+        roundWinner = "Player";
+        console.log(roundWinner);
     }
     // What if player choose paper and computer choose scissor
-    else if (playerSelection == choices[1] && computerSelection == choices[2]) {
-        roundWinner = "Katch! Katch! you lost";
+    else if (playerSelection == 'paper' && computerSelection == choices[2]) {
+        roundWinner = "Computer";
+        console.log(roundWinner);
     }
     // What if player choose scissor and computer choose rock
-    else if (playerSelection == choices[2] && computerSelection == choices[0]) {
-        roundWinner = "Broke right through ya! You lost";
+    else if (playerSelection == 'scissor' && computerSelection == choices[0]) {
+        roundWinner = "Computer";
+        console.log(roundWinner);
     }
     // What if player choose scissor and computer choose paper
-    else if (playerSelection == choices[2] && computerSelection == choices[1]) {
-        roundWinner = "You cut through me this time and win for now";
+    else if (playerSelection == 'scissor' && computerSelection == choices[1]) {
+        roundWinner = "Player";
+        console.log(roundWinner);
     }
+    gamePlay();
 }
 
 
-function gameLoop() {
-    let roundResult
-        , playerChoice
-        , computerChoice
-        , computerPoints = 0
-        , playerPoints = 0;
+function gamePlay() {
 
-    let totalRounds = "";
+    /** This commented out code is for future upgradations!
+
+    // let totalRounds = "";
     // if (isNaN(totalRounds)) {
     //     alert("Please enter a number");
     //     gameLoop() // For restarting the game
     // }
-    if(computerPoints === Math.floor(+totalRounds/2 + 1) || playerPoints ===  Math.floor(+totalRounds/2 + 1)){
-        alert("The maximum amount of total rounds are 10")
-        gameLoop() // For restarting the game
-    }
-    else{
-        for (i = 0; i < totalRounds; i++) {
-            computerChoice = getComputerChoice();
-            playerChoice = getPlayerChoice();
-            playRound(playerChoice,computerChoice);
 
-            if (roundWinner.includes("win")){
-                playerPoints++;
-                console.log(roundWinner);
-            }
-            else if (roundWinner.includes("lost")){
-                computerPoints++;
-                console.log(roundWinner)
-            }
-            else if(roundWinner.includes("draw")){
-                i-- // If the round is declared draw , take back the turn
-                console.log(roundWinner)
-            }
-        }
+    */
 
-        /** IN the statements below , + 1 was done because 
-         * the for loop will end with points at a number less than actual finishing number 
-         */
-        if(computerPoints > playerPoints){
-            console.log("You lost the game by " + (computerPoints - playerPoints))
+    let announceWinner = document.querySelector('.winners');
+    let container = document.querySelector('.container');
+    if(computerPoints === 5){
+        container.textContent = "Computer side WINS!" ;
+    }
+    else if(playerPoints === 5){
+       container.textContent = "Humanity WON!";
+    }
+    else if(playerPoints !== 5 && computerPoints !== 5){
+        if(roundWinner == "Draw"){
+            announceWinner.textContent = "Whoa! Its a draw.";
         }
-        else if(computerPoints < playerPoints){
-            console.log("You won the game by " + (playerPoints - computerPoints))
+        else if(roundWinner == "Player"){
+            announceWinner.textContent = "Human won this round!";
+            playerPoints++;
+            playerScore.textContent = playerPoints;
+
+            /**To not forward to next round even after 5 points on board */
+            if(computerPoints === 5){
+                container.textContent = "Computer side WINS!" ;
+            }
+            else if(playerPoints === 5){
+               container.textContent = "Humanity WON!";
+            }
+        }
+        else if(roundWinner == "Computer"){
+            announceWinner.textContent = "Computers...as always won the race of this round";
+            computerPoints++;
+            computerScore.textContent = computerPoints;
+
+            /**To not forward to next round even after 5 points on board */
+            if(computerPoints === 5){
+                container.textContent = "Computer side WINS!" ;
+            }
+            else if(playerPoints === 5){
+               container.textContent = "Humanity WON!";
+            }
         }
     }
+
 }
-
-
-gameLoop(); // Game will start from here by calling this function
 
 
 
